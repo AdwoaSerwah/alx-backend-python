f35b7987fae519dc1e45ef5f9d7d6a780c1982a8
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-22 19:03:20 +0000

    Add task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function `floor` with type annotations.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function that returns the floor of a given float.
+
+    Args:
+    n (float): The float to be floored.
+
+    Returns:
+    int: The floored value of the float.
+    """
+    return math.floor(n)
