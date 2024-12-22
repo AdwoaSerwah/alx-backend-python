c0716af9aec831beca4b7b82b3bc7770251f9907
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-22 19:05:57 +0000

    Add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function `to_str` with type annotations.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that converts a float to its string representation.
+
+    Args:
+    n (float): The float to be converted.
+
+    Returns:
+    str: The string representation of the float.
+    """
+    return str(n)
