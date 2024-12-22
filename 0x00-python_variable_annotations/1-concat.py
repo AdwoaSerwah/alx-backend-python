ecc9f22867cba665d98c8bcc8d8361f2ad1eccf3
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-22 18:59:10 +0000

    Add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function `concat` with type annotations.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that concatenates two strings and returns the result.
+
+    Args:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+
+    Returns:
+    str: The concatenated string.
+    """
+    return str1 + str2
