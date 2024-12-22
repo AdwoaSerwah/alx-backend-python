99bf6ff3f65f6bff6f21349822f5c8047e21822d
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-22 18:53:48 +0000

    Add task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function `add` with type annotations.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that adds two float numbers and returns the result as a float.
+
+    Args:
+    a (float): The first number to add.
+    b (float): The second number to add.
+
+    Returns:
+    float: The sum of the two input floats.
+    """
+    return a + b
